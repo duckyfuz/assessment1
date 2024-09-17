8526aa545ea246cb05f2ffc5edaa999653d63ebe
Kenneth Gao <108561447+duckyfuz@users.noreply.github.com>
2024-09-17 16:44:21 +0800

    add function to fetch from API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,23 @@
+"use client";
+
 import styles from "./page.module.css";
+
+const fetchUserList = () => {
+  (async function fetchUsers() {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const users = await response.json();
+      console.log(users);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  })();
+};
 
 export default function Home() {
   return <div>Form goes here</div>;
